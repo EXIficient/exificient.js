694f7e30e03101f8e8f22154ee85681bc0425a25
danielpeintner <daniel@peintner.org>
2016-03-10 13:27:11 +0100

    update grammars and slightly code

--- exificient.js
+++ exificient.js
@@ -1,4 +1,4 @@
-/*! exificient.js v0.0.2-SNAPSHOT | (c) 2015 Siemens AG | The MIT License (MIT) */
+/*! exificient.js v0.0.2-SNAPSHOT | (c) 2016 Siemens AG | The MIT License (MIT) */
 
 /* TODO How to realize proper error handling */
 
